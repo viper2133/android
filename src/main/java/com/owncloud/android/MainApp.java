188d27e0af9977b94c0a3e16feccf6ec30c45d27
Mario Danic <mario@lovelyhq.com>
2018-04-11 14:31:45 +0200

    Clean manual uploads on app start
    Signed-off-by: Mario Danic <mario@lovelyhq.com>

--- src/main/java/com/owncloud/android/MainApp.java
+++ src/main/java/com/owncloud/android/MainApp.java
@@ -42,6 +42,7 @@ import android.support.v7.app.AlertDialog;
 import android.text.TextUtils;
 import android.view.WindowManager;
 
+import com.evernote.android.job.Job;
 import com.evernote.android.job.JobManager;
 import com.owncloud.android.authentication.AccountUtils;
 import com.owncloud.android.authentication.PassCodeManager;
@@ -159,6 +160,7 @@ public class MainApp extends MultiDexApplication {
         initSyncOperations();
         initContactsBackup();
         notificationChannels();
+        cleanManualUploads();
 
         // register global protection with pass code
         registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
@@ -362,6 +364,25 @@ public class MainApp extends MultiDexApplication {
         }
     }
     
+
+    private void cleanManualUploads() {
+        ArbitraryDataProvider arbitraryDataProvider = new ArbitraryDataProvider(getContentResolver());
+        boolean jobRunning = false;
+        for (Job job : JobManager.instance().getAllJobs()) {
+            if (!job.isFinished()) {
+                jobRunning = true;
+                break;
+            }
+        }
+        
+        if (!jobRunning) {
+            new Thread(() -> {
+                arbitraryDataProvider.deleteKeyLike("upload_queue_move%");
+                arbitraryDataProvider.deleteKeyLike("upload_queue_delete%");
+                arbitraryDataProvider.deleteKeyLike("upload_queue_nothing%");
+            }).start();
+        }
+    }
 
     public static Context getAppContext() {
         return MainApp.mContext;
